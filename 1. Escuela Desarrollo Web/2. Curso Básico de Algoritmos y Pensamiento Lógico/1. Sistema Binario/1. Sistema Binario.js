8fe1d4ab29622e50b02a5b1aef5454a3a7a36a88
lmanml <mnm.soul@gmail.com>
2022-04-13 14:48:37 -0500

    123

---
+++ 1. Escuela Desarrollo Web/2. Curso Básico de Algoritmos y Pensamiento Lógico/1. Sistema Binario/1. Sistema Binario.js
@@ -0,0 +1,43 @@
+/*
+ ! Reto, traducir año actual - año en que nací en binario para obtener mi edad en binario
+
+
+    *Solución 
+    
+    Año actual  = 11111100110
+    2022/2 -> 0
+    1011/2 -> 1
+     505/2 -> 1
+     252/2 -> 0
+     126/2 -> 0 
+      63/2 -> 1
+      31/2 -> 1
+      15/2 -> 1
+       7/2 -> 1
+       3/2 -> 1
+       1/2 -> 1 <- Stop here and read upwards
+
+    
+    Nacimiento   = 11111001101
+    1997/2 -> 1
+     998/2 -> 0
+     499/2 -> 1
+     249/2 -> 1
+     124/2 -> 0
+      62/2 -> 0
+      31/2 -> 1
+      15/2 -> 1
+       7/2 -> 1
+       3/2 -> 1
+       1/2 -> 1 <- Stop here and read upwards
+
+    
+    Año actual = 11111100110
+    Nacimiento = 11111001101  -
+   --------------------------
+    Mi edad    = 00000011001     <- Leading 0s can be ignored, turning into just -> 11001 -> 25
+
+
+
+
+*/
